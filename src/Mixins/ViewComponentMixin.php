7de5b0b1d52cf7edfb5573e611379d4e53ba474d
Alexander <alex-78910@mail.ru>
2023-04-29 22:16:04 +0700

    fixes

--- src/Mixins/ViewComponentMixin.php
+++ src/Mixins/ViewComponentMixin.php
@@ -4,6 +4,7 @@ namespace Kpebedko22\FilamentTranslation\Mixins;
 
 use Closure;
 use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\Field;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
@@ -20,50 +21,81 @@ class ViewComponentMixin
             string $labelKey = 'common',
             string $placeholderKey = 'placeholder'
         ): self {
-            $class = get_class($this);
+//            $class = get_class($this);
 
-            switch ($class) {
-                case TextInput::class:
-                case Select::class:
-                case Textarea::class:
+            $closure = match (true) {
+                $this instanceof Field => function () use ($path, $labelKey, $placeholderKey) {
 
                     /** @var TextInput|Select|Textarea $this */
 
+                    // TODO: Field doesnt have placeholder
                     $name = $this->getName();
 
                     $this->label(__("$path.$labelKey.$name"))
                         ->placeholder(__("$path.$placeholderKey.$name"));
+                },
 
-                    break;
-                case Checkbox::class:
-                case Toggle::class:
-                    /** @var Checkbox|Toggle $this */
 
-                    $name = $this->getName();
-                    $this->label(__("$path.$labelKey.$name"));
-
-                    break;
-
-                case Column::class:
+                $this instanceof Column => function () use ($path, $labelKey) {
                     /** @var Column $this */
                     $name = $this->getName();
 
                     $this->label(__("$path.$labelKey.$name"));
+                },
 
-                    break;
+                $this instanceof Filter => function () use ($path, $labelKey) {
 
-                case Filter::class:
                     /** @var Filter $this */
 
                     $name = $this->getName();
 
                     $this->label(__("$path.$labelKey.$name"));
+                }
+            };
 
-                    break;
-
-                default:
-                    break;
-            }
+            $closure();
+//            switch ($class) {
+//                case TextInput::class:
+//                case Select::class:
+//                case Textarea::class:
+//
+//                    /** @var TextInput|Select|Textarea $this */
+//
+//                    $name = $this->getName();
+//
+//                    $this->label(__("$path.$labelKey.$name"))
+//                        ->placeholder(__("$path.$placeholderKey.$name"));
+//
+//                    break;
+//                case Checkbox::class:
+//                case Toggle::class:
+//                    /** @var Checkbox|Toggle $this */
+//
+//                    $name = $this->getName();
+//                    $this->label(__("$path.$labelKey.$name"));
+//
+//                    break;
+//
+//                case Column::class:
+//                    /** @var Column $this */
+//                    $name = $this->getName();
+//
+//                    $this->label(__("$path.$labelKey.$name"));
+//
+//                    break;
+//
+//                case Filter::class:
+//                    /** @var Filter $this */
+//
+//                    $name = $this->getName();
+//
+//                    $this->label(__("$path.$labelKey.$name"));
+//
+//                    break;
+//
+//                default:
+//                    break;
+//            }
 
             return $this;
         };
