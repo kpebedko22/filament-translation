54557c6bae39ccc76cca6efcbea1f8d53342f28a
Alexander <alex-78910@mail.ru>
2023-08-06 22:05:18 +0700

    update

--- src/Traits/TranslatableViewRecordPage.php
+++ src/Traits/TranslatableViewRecordPage.php
@@ -4,7 +4,7 @@ namespace Kpebedko22\FilamentTranslation\Traits;
 
 trait TranslatableViewRecordPage
 {
-    protected function getTitle(): string
+    public function getTitle(): string
     {
         return $this->getResource()::translation()->getLabelView();
     }
